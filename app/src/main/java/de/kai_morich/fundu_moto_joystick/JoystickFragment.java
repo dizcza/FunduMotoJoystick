1a742bca02b0bb86a1e29f2a0bca11f6829404a7
dizcza <dizcza@gmail.com>
2019-07-06 22:47:57 +0200

    M<angle><radius> command; log bluetooth

--- app/src/main/java/de/kai_morich/fundu_moto_joystick/JoystickFragment.java
+++ app/src/main/java/de/kai_morich/fundu_moto_joystick/JoystickFragment.java
@@ -1,5 +1,6 @@
 package de.kai_morich.fundu_moto_joystick;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -9,14 +10,18 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.graphics.PointF;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.method.ScrollingMovementMethod;
@@ -31,9 +36,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -54,11 +63,12 @@ public class JoystickFragment extends Fragment implements ServiceConnection, Ser
     private PointF mPointerCenter;
     private final PointF mMoveVector = new PointF(0, 0);
     private Timer mTimer;
-    private static final long SEND_UPDATE_PERIOD = 100;  // ms
+    private static final long SEND_UPDATE_PERIOD = 500;  // ms
     private static final double ANGLE_RESOLUTION = Math.PI / 18;
     private static final int VELOCITY_AMPLITUDE = 100;  // any value less than 127
     private TimerTask mTimerSendTask;
-    private Handler mHandler;
+    private final Handler mHandler;
+    private final File mLogsFile;
 
     private class TimerSendTask extends TimerTask {
 
@@ -71,19 +81,24 @@ public class JoystickFragment extends Fragment implements ServiceConnection, Ser
 
         @Override
         public void run() {
-            byte[] data = new byte[2 + newline.length()];
+            float x, y;
             synchronized (mMoveVector) {
-                double angle = Math.atan2(mMoveVector.y, mMoveVector.x);
-                double radius = Math.sqrt(mMoveVector.x * mMoveVector.x +
-                        mMoveVector.y * mMoveVector.y);
-                data[0] = (byte) (angle / ANGLE_RESOLUTION);
-                data[1] = (byte) (radius * VELOCITY_AMPLITUDE);
+                x = mMoveVector.x;
+                y = mMoveVector.y;
             }
+            // construct command in the format "M<angle><radius>"
+            // 'M' stands for MOVE
+            byte[] data = new byte[3 + newline.length()];
+            double angle = Math.atan2(y, x);
+            double radius = Math.sqrt(x * x + y * y);
+            data[0] = (byte) 'M';
+            data[1] = (byte) (angle / ANGLE_RESOLUTION);
+            data[2] = (byte) (radius * VELOCITY_AMPLITUDE);
             for (int i = 0; i < newline.length(); i++) {
-                data[2 + i] = (byte) newline.charAt(i);
+                data[3 + i] = (byte) newline.charAt(i);
             }
             boolean successful = send(data);
-            final String message = String.format("AngleBin=%d VelocityRadius=%d %b", data[0], data[1], successful);
+            final String message = String.format("AngleBin=%d VelocityRadius=%d %b", data[1], data[2], successful);
             mHandler.post(new Runnable() {
                 @Override
                 public void run() {
@@ -95,6 +110,7 @@ public class JoystickFragment extends Fragment implements ServiceConnection, Ser
 
     public JoystickFragment() {
         mHandler = new Handler();
+        mLogsFile = getLogsFile();
     }
 
     /*
@@ -124,6 +140,7 @@ public class JoystickFragment extends Fragment implements ServiceConnection, Ser
             service.attach(this);
         else
             getActivity().startService(new Intent(getActivity(), SerialService.class)); // prevents service destroy on unbind from recreated activity caused by orientation change
+        requestsStoragePermission();
     }
 
     @Override
@@ -166,6 +183,7 @@ public class JoystickFragment extends Fragment implements ServiceConnection, Ser
     public void onPause() {
         super.onPause();
         mTimerSendTask.cancel();
+        mHandler.removeCallbacks(mTimerSendTask);
     }
 
     @Override
@@ -273,12 +291,7 @@ public class JoystickFragment extends Fragment implements ServiceConnection, Ser
 
     private boolean send(byte[] data) {
         if (connected != Connected.True) {
-            mHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(getActivity(), "not connected", Toast.LENGTH_SHORT).show();
-                }
-            });
+            // TODO notify user via separate BL status icon
             return false;
         }
         boolean success;
@@ -299,6 +312,30 @@ public class JoystickFragment extends Fragment implements ServiceConnection, Ser
 
     private void receive(byte[] data) {
         receiveText.append(new String(data));
+        if (!isExternalStorageWritable()) {
+            return;
+        }
+        logBluetooth(data);
+    }
+
+    private boolean logBluetooth(byte[] data) {
+        Date now = new Date();
+        long timestamp = now.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("[mm:ss.SSSS] ", Locale.US);
+        String dateStr = sdf.format(timestamp);
+        String s = dateStr + new String(data);
+//        data = s.getBytes();
+        boolean success;
+        try {
+            FileOutputStream fileOutput = new FileOutputStream(mLogsFile, true);
+            fileOutput.write(data);
+            fileOutput.close();
+            success = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            success = false;
+        }
+        return success;
     }
 
     private void status(String str) {
@@ -356,6 +393,26 @@ public class JoystickFragment extends Fragment implements ServiceConnection, Ser
     public void onSerialIoError(Exception e) {
         status("connection lost: " + e.getMessage());
         disconnect();
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+    }
+
+    public static File getLogsFile() {
+        return new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOCUMENTS), "FunduMoto_Logs.txt");
+    }
+
+    public void requestsStoragePermission() {
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MainActivity.PERMISSION_WRITE_STORAGE_REQUEST_CODE);
+        }
     }
 
 }
